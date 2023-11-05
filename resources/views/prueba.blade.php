f12b88865cb98f6b81c355a4b3fc76467de558ce
annakary <59904505+annakary@users.noreply.github.com>
2023-11-05 16:56:50 -0700

    nuevas rutas y user controller

---
+++ resources/views/prueba.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <h1> Hola <?php echo $name; ?> </h1> 
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac tempor leo. Aenean ultricies lorem vel sapien condimentum, ut lobortis diam consequat. Vestibulum ac egestas lacus. Etiam quis ante et libero mattis pulvinar. Sed vel augue nunc. Etiam varius magna vel nisl aliquet, sed aliquam nulla vehicula. Ut sed semper neque, sed maximus orci. Nullam vulputate eros sit amet justo pellentesque, nec accumsan mi elementum. Morbi id ligula nec urna imperdiet sollicitudin quis sed metus. Fusce finibus porta porttitor.
+
+Praesent orci leo, viverra at interdum laoreet, hendrerit mattis velit. Ut id convallis quam, eu sagittis mi. Duis eget sagittis eros. Ut in placerat neque. Maecenas eu odio mauris. Aliquam iaculis id ligula in mollis. Aliquam suscipit erat a eros dapibus, ac feugiat odio luctus. Donec placerat velit eget mollis consequat. Nunc quis nunc facilisis, tempor diam in, accumsan turpis. Morbi vitae arcu imperdiet, venenatis sapien a, euismod libero. Vivamus finibus odio quis accumsan ultricies.</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,60 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// ruta que requiere parametros}
+
+// Route::get('/buscar-user/{id}', function (string $id) {
+//     return 'User '.$id;
+// });
+
+// rutas opcionales
+ Route::get('/saludar/{name}/{lastname?}', function (string $name, $lastname = "vega") {
+    if (preg_match('/^[A-Za-z]+$/', $name) && preg_match('/^[A-Za-z]+$/', $lastname)) {
+        return 'Hola ' . $name . ' ' . $lastname;
+    } else {
+        return 'Por favor, ingresa solo letras del alfabeto.';
+    }
+  });
+
+// practicamente tomas la url y devuelves lo que quieras sea suma, resta, etc
+######################  SUMA  ##########################
+ Route::get('/suma/{num1}/{num2}', function ($num1, $num2) {
+    if (is_numeric($num1) && is_numeric($num2)) {
+        return $num1 + $num2;
+    } else {
+        return "Por favor, ingresa solo números.";
+    }
+ });
+ ######################  RESTA  ##########################
+Route::get('/resta/{num1}/{num2}', function ($num1, $num2) {
+    if (is_numeric($num1) && is_numeric($num2)) {
+        return $num1 + $num2;
+    } else {
+        return "Por favor, ingresa solo números.";
+    }
+ });
+ ######################  MULTIPLICACION  ##########################
+  Route::get('/multiplica/{num1}/{num2}', function ($num1, $num2) {
+    if (is_numeric($num1) && is_numeric($num2)) {
+        return $num1 + $num2;
+    } else {
+        return "Por favor, ingresa solo números.";
+    }
+  });
+  ######################  DIVISION  ##########################
+  Route::get('divide/{num1}/{num2}', function ($num1, $num2) {
+    if (is_numeric($num1) && is_numeric($num2)) {
+        return $num1 + $num2;
+    } else {
+        return "Por favor, ingresa solo números.";
+    }
+  });
+  
+
+// Route::get('/vista/{name}', function ($name) {
+//     return view('prueba', ["name" => $name]);
+// });
+
+Route::get('/prueba-controller/{name}', [UserController::class,'index']);
+
+
